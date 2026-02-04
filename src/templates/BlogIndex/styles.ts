d212bb00e100aa4239128566cf925ed6175a6a44
Mark Northcott <mark.northcott@gmail.com>
2026-02-04 18:07:13 -0500

    Migrate Gatsby pages to Eleventy templates and cleanup

--- src/templates/BlogIndex/styles.ts
+++
@@ -1,9 +0,0 @@
-import tw, {styled} from 'twin.macro';
-
-export const Links = styled.div`
-  ${tw`w-full flex justify-between mt-4 text-sm`}
-`;
-
-export const Link = styled.span`
-  ${tw`text-blue-500 shadow-link`}
-`;
